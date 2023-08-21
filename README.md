21c2dbe812e36a04143e1df1f0a406c6365efd85
Jennifer Alvarez <alvarezjennifer472@gmail.com>
2023-08-21 17:00:35 -0500

    bulk update final

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # sqlalchemy-challenge
 Mod 10 Advanced SQL Homework Challenge
 
+- Resources: Utilized the in-class activities as a reference point.
+
 - References
 Menne, M.J., I. Durre, R.S. Vose, B.E. Gleason, and T.G. Houston, 2012: An overview of the Global Historical Climatology Network-Daily Database. Journal of Atmospheric and Oceanic Technology, 29, 897-910, https://journals.ametsoc.org/view/journals/atot/29/7/jtech-d-11-00103_1.xml
--- SurfsUp/app-JALVAREZ.py
+++ SurfsUp/app-JALVAREZ.py
@@ -34,7 +34,6 @@ session = Session(bind=engine)
 app = Flask(__name__)
 
 
-
 #################################################
 # Flask Routes
 #################################################
@@ -46,10 +45,78 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start>"
-        f"/api/v1.0/<start>/<end>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end<br/>"
     )
 
+### Precipitation route
 @app.route("/api/v1.0/precipitation")
 
 def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all precipitation data"""
+    
+    # Query alll precipitation data
+    results = session.query(measurement.date, measurement.prcp).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_precip
+    all_precip = []
+    for date, prcp in results:
+        precipitation_dict = {}
+        precipitation_dict['Date'] = date
+        all_precip.append(precipitation_dict)
+
+    return jsonify(all_precip)
+
+
+
+### Stations route
+@app.route("/api/v1.0/stations")
+
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all station data"""
+    
+    # Query alll station data
+    results = session.query(station.station, station.name, station.latitude, station.longitude, station.elevation).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_stations
+    all_stations = []
+    for station, name, latitude, longitude, elevation in results:
+        station_dict = {}
+        station_dict['station'] = station
+        station_dict['name'] = name
+        station_dict['latitude'] = latitude
+        station_dict['longitude'] = longitude
+        station_dict['elevation'] = elevation
+        all_stations.append(station_dict)
+
+    return jsonify(all_stations)
+
+
+# ### TOBS route
+# @app.route("/api/v1.0/tobs")
+
+# def tobs():
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
+
+#     """Return a list of all TOBS data fir station USC00519281"""
+    
+#     # Query alll station data
+#     results = session.query(station.station, station.name, station.latitude, station.longitude, station.elevation).all()
+
+#     session.close()
+
+#     # Create a dictionary from the row data and append to a list of all_stations
+
+
+### note to grader:  will submit for feedback and meet with a TA/tutor to troubleshoot. will make use of multiple submission attempts.
